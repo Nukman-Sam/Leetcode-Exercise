7160b58acb2f78ae05e9a521e8c76679c7a9b067
Nukman_Sam <48399850+Nukman-Sam@users.noreply.github.com>
2026-06-09 16:09:53 +0800

    Update Leetcode3.cpp

--- Leetcode3.cpp
+++ Leetcode3.cpp
@@ -8,82 +8,61 @@
 #include <string>
 #include <stdio.h>
 #include <cstring>
+#include <vector>
 
 using namespace std;
-
-int main()
-{
+int main() {
+    // variables
+    int same_number = 0 ;
+    int string_count = 1 ;
+    int max_count = 1;
+    int repeat_num = 0;
+    
     
     //Word or names variables
     string name_input = "";
-    
     
     cout<<"Insert name/word" << endl;
     
     getline(cin,name_input);
     
-    int name_length = name_input.length();
+    // Create a vector called cars that will store strings
+    vector<char> cars(name_input.begin(), name_input.end());
     
-    cout<<"the word is :" << name_input << endl;
-    cout<<"the length is :" << name_length << endl << endl;
-    
-    
-    
-    // Array to insert the string 
-    char alphabet_count[name_length];
-    
-    // char for indicator and counter(s) to count the sequence
-    char alphabet_marker; // to mark new alphabet 
-    char char_indicator; // alphabet indicator to compare with alphabet_marker
-    
-    int substring_counter = 1;
-    int max_sequence = 1;
-    
-    // Copy string to array of char
-    strcpy(alphabet_count,name_input.c_str());
-    
-    //Test output
-    cout<<"String to char array conversion:\n";
-	for (int i = 0; i < name_length; i++) 
-		    cout << alphabet_count[i]; 
 
+	// Leetcode part
+    same_number = cars[0];
+    for(int count = 0; count < cars.size(); count++)
+    {
+        cout << cars[count];
+        
+        
+        if (cars[count] == same_number)
+        {
+            cout << " reset" << endl;
+            string_count = 1;
+            same_number = cars[count];
+            continue;
+        }
+        else 
+        {   
+            
+            if(cars[count] == cars[count-1] || cars[count] == cars[count-2] || cars[count] == cars[count-3] || cars[count] == cars[count-4] || cars[count] == cars[count-5])
+                {
+                 cout << " Same" << endl;
+                 string_count--;
+                 continue;   
+                }
+                
+            string_count++;
+            if (string_count > max_count)
+                max_count = string_count;
+        }
+        
+        cout << endl;
+    }
+    
+    cout << endl << "max count is :" << max_count;
 
-    cout << "\n\n";
-    
-    
-    // Leetcode Exercise part
-    
-    char_indicator = alphabet_count[0];
-    alphabet_marker = char_indicator;
-    for (int counter = 1; counter < name_length; counter++) 
-		   { 
-		       char_indicator = alphabet_count[counter];
-		       if (alphabet_marker == char_indicator)
-		       {
-		           substring_counter = 1;
-		           cout<<"sama " << endl;
-		           alphabet_marker = char_indicator;
-		       }
-		       
-		       else
-		       {
-		           if (substring_counter == max_sequence)
-		              max_sequence++;
-		           
-		           
-		           if (alphabet_count[counter] == alphabet_count[counter-1])    
-		              cout<<"repeat " << endl;
-		           
-		           substring_counter++;
-		           cout<<"lain " << endl;
-		       }
-		       
-		      
-		       
-               
-		   }
-		   
-		 	cout<<"The longest substring is " << max_sequence << endl;
-	
     return 0;
 }
